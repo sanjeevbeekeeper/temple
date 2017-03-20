f686adc625c58c6ef5d78b3d042c086e29bdb407
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-03-20 13:16:29 +0530

    Deleted the tested output.

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php get_header(); ?>
 <!-- header -->
 
-    <?php
-        $address_01 = get_post_meta($post->ID, 'address01', true);
-        $address_02 = get_post_meta($post->ID, 'address02', true);
-     ?>
-    <p>
-        <?php echo $address_01; ?>
-        <?php echo $address_02; ?>
-    </p>
-
-
-
-
-
 <!-- Loop start -->
 <?php
     if ( have_posts() ) :
