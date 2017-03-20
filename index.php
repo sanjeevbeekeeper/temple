c95614900d5fbd9ba2ffe559c3f8a46c67f3380b
Sanjeev Beekeeper PC home <sanjeevramanathan@gmail.com>
2017-03-20 13:16:12 +0530

    testing the output from the dashboard.

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <?php get_header(); ?>
 <!-- header -->
 
+    <?php
+        $address_01 = get_post_meta($post->ID, 'address01', true);
+        $address_02 = get_post_meta($post->ID, 'address02', true);
+     ?>
+    <p>
+        <?php echo $address_01; ?>
+        <?php echo $address_02; ?>
+    </p>
+
+
+
+
+
 <!-- Loop start -->
 <?php
     if ( have_posts() ) :
     while ( have_posts() ) : the_post();
+    ?>
 
+<?php
         get_template_part('temple-parts/content', get_post_format());
 
     // Loop end
